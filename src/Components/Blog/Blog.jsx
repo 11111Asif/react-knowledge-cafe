bc938977c6492a367041ce76e3164e187442211e
11111Asif <asif040502@gmail.com>
2024-03-12 23:58:05 +0600

    added a react html

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -2,10 +2,28 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-
+const {title, cover, reading_time, author_img, author, posted_date, hashtags} = blog
     return (
         <div>
-            
+            <img src={cover} alt={`cover picture of the title ${title}`}></img>
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img}></img>
+                    <div className='ml-6'>
+                        <h2 className='text-2xl'>{author}</h2>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-3xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx)=><span key={idx}><a href=''>#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
